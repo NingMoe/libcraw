ae5ce6dcd811eb0902dcc8bd847852dbaa993cc2
jacelynfish <jacelynfish@outlook.com>
2016-05-04 21:38:32 +0800

    Update app.js

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ var crawler = require('./routes/libCrawler');
 var app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
 
 app.set('port',process.env.PORT || 3000);
 
