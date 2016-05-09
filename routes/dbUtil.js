2147da45e198bbb343f3fdeac81fe94ecad5607b
jacelynfish <jacelynfish@outlook.com>
2016-05-09 16:02:41 +0800

    Update dbUtil.js

--- routes/dbUtil.js
+++ routes/dbUtil.js
@@ -1,76 +1,79 @@
 var MongoClient = require('mongodb').MongoClient;
 var dbPath = 'mongodb://localhost:27017/students';
-var request = require('request');
+
 var cheerio = require('cheerio');
 var url = 'https://coes-stud.must.edu.mo/coes/login.do';
 
-var dbUtil = {
-    stuInfoLogin : function(value,msg,wechat){
+var dbUtil = function(){
+    this.stuInfoLogin = function(value,msg,wechat){
+        var request = require('request');
         var stuID = value.split(' ')[0];
         var stuPWD = value.split(' ')[1];
         var openID = msg.toUserName;
 
         //validate whether the student info are correct
         var resMsgContent = '';
-        var htmltoken = '';
 
         request(url,function(err,res,body){
-            var $ = cheerio.load(body,{normalizeWhitespace: true});
-            htmltoken = $('input[name="org.apache.struts.taglib.html.TOKEN"]').attr('name');
-        })
-        var options = {
-            url:url,
-            headers:{
-                'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-                'Accept-Encoding':'gzip, deflate',
-                'Accept-Language':'zh-CN,zh;q=0.8',
-                'Cache-Control':'max-age=0',
-                'Connection':'keep-alive',
-                'Content-Type':'application/x-www-form-urlencoded'
-            },
-            form:{
-                'userid':stuID,
-                'password':stuPWD,
-                'org.apache.struts.taglib.html.TOKEN':htmltoken
-            },
-            jar:true
-
-        }
-
-
-        request.post(options,callback);
-
-        function callback(err, res, body){
-
-            if(!err && res.statusCode === 200){
+            if(!err && res.statusCode == 200){
                 var $ = cheerio.load(body,{normalizeWhitespace: true});
-                var index = $('title').text().indexOf('Inbox');
-                console.log($('title').text());
-                console.log(index);
-                if(index!=-1){
+                var token = $('input[name="org.apache.struts.taglib.html.TOKEN"]').attr('value');
+                console.log('token: '+ token);
 
-                    request.get({
-                        url:'https://coes-stud.must.edu.mo/coes/logout.do',
-                        jar:true
-                    })
-                    resMsgContent = '您已成功绑定学生卡号信息';
-                    dbConnection(dbPath,stuID,stuPWD,openID);
-                    wechatSendMsg(msg,resMsgContent,wechat);
-                }else{
-                    request.get({
-                        url:'https://coes-stud.must.edu.mo/coes/logout.do',
-                        jar:true
-                    })
-                    resMsgContent = '绑定失败，可能由以下原因之一造成绑定失败\n' +
-                        '【1】学生卡号、密码不正确\n' +
-                        '【2】此学生卡号并未正常退出选课系统\n' +
-                        '【3】卡号密码中间没有由空格隔开如\"【绑定】1409853G-A123-B4567 12345678\"';
-                    wechatSendMsg(msg,resMsgContent,wechat);
+                var options = {
+                    url:url,
+                    headers:{
+                        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                        'Accept-Encoding':'gzip, deflate',
+                        'Accept-Language':'zh-CN,zh;q=0.8',
+                        'Cache-Control':'max-age=0',
+                        'Connection':'keep-alive',
+                        'Content-Type':'application/x-www-form-urlencoded'
+                    },
+                    form:{
+                        'userid':stuID,
+                        'password':stuPWD,
+                        'org.apache.struts.taglib.html.TOKEN':token
+                    },
+                    jar:true
+
                 }
 
 
+                request.post(options,callback);
+
+                function callback(err, res, body){
+
+                    if(!err && res.statusCode === 200){
+                        var $ = cheerio.load(body,{normalizeWhitespace: true});
+                        var index = $('title').text().indexOf('Inbox');
+                        if(index!=-1){
+
+                            request.get({
+                                url:'https://coes-stud.must.edu.mo/coes/logout.do',
+                                jar:true
+                            })
+                            resMsgContent = '您已成功绑定学生卡号信息';
+                            dbConnection(dbPath,stuID,stuPWD,openID);
+                            wechatSendMsg(msg,resMsgContent,wechat);
+                        }else{
+                            request.get({
+                                url:'https://coes-stud.must.edu.mo/coes/logout.do',
+                                jar:true
+                            })
+                            resMsgContent = '绑定失败，可能由以下原因之一造成绑定失败\n' +
+                                '【1】学生卡号、密码不正确\n' +
+                                '【2】此学生卡号并未正常退出选课系统\n' +
+                                '【3】卡号密码中间没有由空格隔开如\"【绑定】1409853G-A123-B4567 12345678\"';
+                            wechatSendMsg(msg,resMsgContent,wechat);
+                        }
+
+
+                    }
+                }
+
             }
-        }
+        })
 
     }
 };
